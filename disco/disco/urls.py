649d041a92f96d6aa27bf8cd8e355c816d5b67b9
LepkoQQ <lepko.san@gmail.com>
2023-06-09 16:26:40 +0200

    mobile page done

--- disco/disco/urls.py
+++ disco/disco/urls.py
@@ -1,16 +1,15 @@
 from django.conf import settings
-from django.urls import include, path
 from django.contrib import admin
-
-from wagtail.admin import urls as wagtailadmin_urls
+from django.urls import include, path
 from wagtail import urls as wagtail_urls
+from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
+    path("", include("home.urls")),
 ]
 
 
