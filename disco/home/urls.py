dd8dc050dcbae21762b8e06a15bf69b6a47ea46d
LepkoQQ <lepko.san@gmail.com>
2023-06-14 16:31:37 +0200

    fix page url

--- disco/home/urls.py
+++ disco/home/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("confirm-email", ConfirmEmailView.as_view(), name="confirm-email"),
+    path("confirm-email/", ConfirmEmailView.as_view(), name="confirm-email"),
 ]
